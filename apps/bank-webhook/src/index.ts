ffe09eb5fc7e4ed09476eb1cfdb55f89b0af69e9
Bandi Revanth <bandirevanth143@gmail.com>
2024-09-20 15:30:36 +0530

    Added Bank-webHook Server

---
+++ apps/bank-webhook/src/index.ts
@@ -0,0 +1,54 @@
+import db from "@repo/db/client";
+import express from "express";
+import { z } from "zod";
+
+const app = express();
+app.use(express.json());
+
+const InputTypes = z.object({
+  token: z.string(),
+  amount: z.number(),
+  userId: z.string(),
+});
+
+app.post("/hdfcWebhook", async (req, res) => {
+  const payLoad = InputTypes.safeParse(req.body);
+  if (!payLoad.success) {
+    res.status(411).json("Inputs are not correct");
+    return;
+  }
+  try {
+    await db.$transaction([
+      db.balance.updateMany({
+        where: {
+          userId: payLoad.data.userId,
+        },
+        data: {
+          amount: {
+            increment: payLoad.data.amount,
+          },
+        },
+      }),
+
+      db.onRampTransactions.updateMany({
+        where: {
+          token: payLoad.data.token,
+        },
+        data: {
+          amount: payLoad.data.amount,
+          provider: "hdfc",
+          status: "success",
+        },
+      }),
+    ]);
+
+    res.status(200).json({ msg: "captured" });
+  } catch (e) {
+    console.log(e);
+    res.status(411).json({ msg: "Refund the amount" });
+  }
+});
+
+app.listen(3002, () => {
+  console.log("Server listening on port 3002");
+});
